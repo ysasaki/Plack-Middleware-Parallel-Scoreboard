ba147f4b136be1fd2dd35d8e95a7684406a9f662
Yoshihiro Sasaki <aloelight@gmail.com>
2010-07-14 20:06:12 +0900

    initial commit

---
+++ lib/Plack/Middleware/Parallel/Scoreboard.pm
@@ -0,0 +1,32 @@
+package Plack::Middleware::Parallel::Scoreboard;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::Parallel::Scoreboard -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::Parallel::Scoreboard;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Parallel::Scoreboard is
+
+=head1 AUTHOR
+
+aloelight E<lt>aloelight {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
